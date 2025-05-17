780301cb6b21f6a59411f45309a13af05f3b4fa5
DarekParodia <darekparodia@gmail.com>
2025-05-17 14:13:30 +0200

    refactor: update get_datapoints method to use instance variable

--- custom_components/greenthing/api.py
+++ custom_components/greenthing/api.py
@@ -9,12 +9,12 @@ class ApiHandler:
         """Initialize the API handler."""
         self.api_url = api_url
 
-    async def get_datapoints(api_url: str) -> list:
+    async def get_datapoints(self) -> list:
         """Fetch the list of datapoints from the API."""
         async with aiohttp.ClientSession() as session:
             try:
                 async with async_timeout.timeout(10):
-                    async with session.get(api_url) as response:
+                    async with session.get(self.api_url) as response:
                         if response.status == 200:
                             data = await response.json()
                             return data["dataPoints"]  # Note: case sensitive "dataPoints"
@@ -26,7 +26,7 @@ class ApiHandler:
 
     async def getDatapoint(self, datapoint_name: str):
         """Get the state of a datapoint."""
-        datapoints = await self.get_datapoints(self.api_url)
+        datapoints = await self.get_datapoints()
         for datapoint in datapoints:
             if datapoint["name"] == datapoint_name:
                 return datapoint
